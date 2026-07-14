fd0781af9273789fdfb1a11027822edbd415a2ca
race004 <57238092+race004@users.noreply.github.com>
2026-07-14 20:53:07 +0200

    Add files via upload

---
+++ Cudoviste.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Igra1
+{
+    public class Cudoviste
+    {
+        public string ime { get; set; }
+        public int maxHP { get; set; }
+        public int trenutanHP { get; set; }
+        public int ATK { get; set; }
+
+        public Cudoviste(string ime, int maxHP, int atk)
+        {
+            this.ime = ime;
+            this.maxHP = maxHP;
+            this.ATK = atk;
+
+            trenutanHP = maxHP;
+        }
+
+        public void PrimiUdarac(int steta)
+        {
+            trenutanHP -= steta;
+            if (trenutanHP < 0) 
+                trenutanHP = 0;
+        }
+
+        public bool JeZiv()
+        {
+            return trenutanHP <= 0;
+        }
+
+        public void IspisOsnovni()
+        {
+            Console.WriteLine($"=== {ime} ===");
+            Console.WriteLine($"HP: {trenutanHP}/{maxHP}");
+            Console.WriteLine($"ATK: {ATK}");
+        }
+
+        public void IspisBorba()
+        {
+            Console.WriteLine($"{ime} (HP: {trenutanHP}/{maxHP})");
+        }
+    }
+}
